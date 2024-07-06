78025fff8c0584e916fc02832f0ef964548a4b66
raihan-octopi <raihan.octopi@gmail.com>
2024-07-06 13:35:24 +0600

    project initialized for set up testin

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <div className='bg-red-500'>
+     This is a test LMS project for octopi digital
+    </div>
+  )
+}
+
+export default App
